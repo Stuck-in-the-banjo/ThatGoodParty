699d5346ea5cbe14877ee9e668fc0be9a534cd2f
Fran <fran152job@gmail.com>
2018-05-19 19:19:12 +0200

    Implemented dialogue system

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+
+    public Animator animator;
+
+    public void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.NPC_name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        //dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+    public void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+        Debug.Log("Ending conversation");
+    }
+}
