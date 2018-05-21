7b9e1b6e224dbe895aed4e255377bdacc560c28a
Fran <fran152job@gmail.com>
2018-05-21 18:16:06 +0200

    Implemented main menu

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -8,7 +8,7 @@ public class ButtonManager : MonoBehaviour {
     public void NewGameBtn(string newGameLevel)
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("SpanishScene");
     }
 
     public void ExitgameBtn()
