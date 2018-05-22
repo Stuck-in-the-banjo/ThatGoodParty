55b91b508422830ad3e2f24424d6f4966abc5ace
Fran <fran152job@gmail.com>
2018-05-22 17:02:06 +0200

    Implemented PauseManu with xbox controller

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -16,7 +16,7 @@ public class PauseMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown("joystick button 7"))
         {
             audioSource.Play();
             if (pauseCanvas.isActiveAndEnabled)
