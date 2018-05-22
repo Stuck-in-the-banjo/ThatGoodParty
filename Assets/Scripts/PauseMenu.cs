a9ad1c5413b33e8c984fbe9f8d2a3a18a1f26730
Fran <fran152job@gmail.com>
2018-05-22 18:33:18 +0200

    Polishing UI

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,10 +8,34 @@ public class PauseMenu : MonoBehaviour {
 
     public AudioSource audioSource;
     public Canvas pauseCanvas;
+
+    // Pause Buttons
+    public Button resume;
+    public Image resumeImg;
+    public Button restart;
+    public Image buttonImg;
+    public Button mainMenu;
+    public Image mainmenuImg;
+    public Button exitGame;
+    public Image exitImg;
+
+    private List<Button> buttonList;
+    private List<Image> imageList;
+
+    private bool onPause = false;
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        buttonList = new List<Button>();
+        imageList = new List<Image>();
+
+        buttonList.Add(resume);
+        buttonList.Add(restart);
+        buttonList.Add(mainMenu);
+        buttonList.Add(exitGame);
+
+        imageList.Add(resumeImg);
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -21,6 +45,7 @@ public class PauseMenu : MonoBehaviour {
             audioSource.Play();
             if (pauseCanvas.isActiveAndEnabled)
             {
+                onPause = false;
                 Time.timeScale = 1;
                 pauseCanvas.gameObject.SetActive(false);
             }
@@ -28,8 +53,28 @@ public class PauseMenu : MonoBehaviour {
             {
                 Time.timeScale = 0;
                 pauseCanvas.gameObject.SetActive(true);
+                onPause = true;
             }
 
+        }
+        // Menu navigation
+        if(onPause)
+        {
+            // Make action
+            if(Input.GetKeyDown(KeyCode.JoystickButton0) || Input.GetKeyDown(KeyCode.Q))
+            {
+
+            }
+            // Up
+            else if(Input.GetAxis("Vertical") > 0.0f)
+            {
+                
+            }
+            // Down
+            else if (Input.GetAxis("Vertical") < 0.0f)
+            {
+
+            }
         }
     }
     public void ResumeGame()
