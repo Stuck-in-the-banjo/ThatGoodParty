7e6ea515a70f5c8842766de6aa2345ce1062de21
Fran <fran152job@gmail.com>
2018-05-22 19:42:14 +0200

    Implemented Controller on pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -13,7 +14,7 @@ public class PauseMenu : MonoBehaviour {
     public Button resume;
     public Image resumeImg;
     public Button restart;
-    public Image buttonImg;
+    public Image restartImg;
     public Button mainMenu;
     public Image mainmenuImg;
     public Button exitGame;
@@ -21,6 +22,8 @@ public class PauseMenu : MonoBehaviour {
 
     private List<Button> buttonList;
     private List<Image> imageList;
+
+    private int indice = 0;
 
     private bool onPause = false;
 	// Use this for initialization
@@ -35,6 +38,9 @@ public class PauseMenu : MonoBehaviour {
         buttonList.Add(exitGame);
 
         imageList.Add(resumeImg);
+        imageList.Add(restartImg);
+        imageList.Add(mainmenuImg);
+        imageList.Add(exitImg);
     }
 	
 	// Update is called once per frame
@@ -53,6 +59,7 @@ public class PauseMenu : MonoBehaviour {
             {
                 Time.timeScale = 0;
                 pauseCanvas.gameObject.SetActive(true);
+                imageList[0].gameObject.SetActive(true);
                 onPause = true;
             }
 
@@ -60,23 +67,62 @@ public class PauseMenu : MonoBehaviour {
         // Menu navigation
         if(onPause)
         {
-            // Make action
-            if(Input.GetKeyDown(KeyCode.JoystickButton0) || Input.GetKeyDown(KeyCode.Q))
-            {
-
-            }
             // Up
-            else if(Input.GetAxis("Vertical") > 0.0f)
+            if (Input.GetAxis("Vertical") > 0.0f)
             {
-                
+                if(indice == 0)
+                {
+                    imageList[indice].gameObject.SetActive(false);
+                    indice = (imageList.Count-1);
+                    imageList[indice].gameObject.SetActive(true);
+                }
+                else
+                {
+                    imageList[indice].gameObject.SetActive(false);
+                    indice--;
+                    imageList[indice].gameObject.SetActive(true);
+                }
             }
             // Down
             else if (Input.GetAxis("Vertical") < 0.0f)
             {
-
+                if (indice == (imageList.Count-1))
+                {
+                    imageList[indice].gameObject.SetActive(false);
+                    indice = 0;
+                    imageList[indice].gameObject.SetActive(true);
+                }
+                else
+                {
+                    imageList[indice].gameObject.SetActive(false);
+                    indice++;
+                    imageList[indice].gameObject.SetActive(true);
+                }
+            }
+            // Make action
+            if (Input.GetKeyDown(KeyCode.JoystickButton0) || Input.GetKeyDown(KeyCode.Q))
+            {
+                if(buttonList[indice] == resume)
+                {
+                    ResumeGame();
+                }
+                else if(buttonList[indice] == restart)
+                {
+                    SceneManager.LoadScene("SpanishScene");
+                }
+                else if(buttonList[indice] == mainMenu)
+                {
+                    Time.timeScale = 1;
+                    SceneManager.LoadScene("MainMenu");
+                }
+                else if(buttonList[indice] == exitGame)
+                {
+                    Application.Quit();
+                }
             }
         }
     }
+
     public void ResumeGame()
     {
         Time.timeScale = 1;
