c33fad6bd773f03329d728bec603a043b4eb4204
Fran <fran152job@gmail.com>
2018-06-11 04:38:41 +0200

    Added distorsion shader effect!

---
+++ Assets/Scripts/ImageEffect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ImageEffect : MonoBehaviour
+{
+    public Material EffectMaterial;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (EffectMaterial != null)
+            Graphics.Blit(src, dst, EffectMaterial);
+    }
+}
