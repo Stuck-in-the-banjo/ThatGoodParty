50d16964ad7a5a45867a49e8521838a7be309595
Fran <fran152job@gmail.com>
2018-06-11 15:27:29 +0200

    Fixed pause bug

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -42,6 +42,7 @@ public class PauseMenu : MonoBehaviour {
         imageList.Add(restartImg);
         imageList.Add(mainmenuImg);
         imageList.Add(exitImg);
+        Time.timeScale = 1;
     }
 	
 	// Update is called once per frame
