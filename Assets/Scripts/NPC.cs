864907ec7ec8abfeccc8e7ce2df2d8762ef2e033
Fran <fran152job@gmail.com>
2018-06-18 23:19:04 +0200

    When you talk twice with a NPC will answer shortly

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -6,6 +6,8 @@ public class NPC : MonoBehaviour {
 
     public Player player_state;
     public Dialogue[] dialogues;
+    public Dialogue[] secondRound;
+    public bool talked = false;
     public int turn_to_talk;
 
     private void Start()
@@ -16,7 +18,13 @@ public class NPC : MonoBehaviour {
     public void TriggerDialogue()
     {
 
-        if(turn_to_talk == (int)player_state.player_trips)
+        if(turn_to_talk == (int)player_state.player_trips && talked == false)
+        {
             FindObjectOfType<DialogueManager>().StartDialogue(dialogues[0]);
+            talked = true;
+        }
+        else
+            FindObjectOfType<DialogueManager>().StartDialogue(secondRound[0]);
+
     }
 }
