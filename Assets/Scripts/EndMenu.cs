a4e66dc2dc0b8ec1f6a6700afc7d684517cd5978
Fran <fran152job@gmail.com>
2018-07-11 13:57:17 +0200

    Minum bugs solved

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -135,6 +135,7 @@ public class EndMenu : MonoBehaviour
             }
             else if (buttonList[indice] == exitGame)
             {
+                Application.OpenURL("https://botttos.itch.io/thatgoodparty");
                 Application.Quit();
             }
             else if (buttonList[indice] == franTwitter)
